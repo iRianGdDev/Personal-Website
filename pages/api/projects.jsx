452721627ae06ccf7cf58f3ce7cedd711ab9c2c3
iRian <95495922+iRianGdDev@users.noreply.github.com>
2025-06-14 12:13:27 +0300

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -1,7 +1,7 @@
 export default async (req, res) => {
   const data = [
     {
-      id: 2,
+      id: 1,
       image: "https://ugc.production.linktr.ee/6RJk9s2pQZ2yAdcxw3Ir_TXX2dLlNmwM2OFdf?io=true&size=avatar-v3_0",
       name: "Social Accounts",
       description: "Hello, I am Efe, I am 19 years old, I am a designer, editor, beat maker, writer, animator and software developer.",
